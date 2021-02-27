06d8b77bafd5d52e7ad760a67066fd41b3687bbe
lukas.a.pellegrini@gmail.com <lukas.a.pellegrini@gmail.com>
2021-02-27 17:09:40 +0100

    added url

---
+++ dcfe/__main__.py
@@ -0,0 +1,24 @@
+import sys, getopt, io, re
+import requests, logging, time
+import json, csv
+
+from .class_tracker import My_DcTracker_Class
+from .func_arg import my_argument_function
+
+def main():
+    print('in main')
+    args = sys.argv[1:]
+    print('count of args :: {}'.format(len(args)))
+
+    for arg in args:
+        print('passed argument :: {}'.format(arg))
+
+    argu = my_argument_function(sys.argv[1:])
+
+    my_object = My_DcTracker_Class(argu)
+    
+
+if __name__ == '__main__':
+    main()
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+
 from setuptools import setup
 setup(
         name = 'dcfe',
-        version = '0.0.1',
+        version = '0.1.0',                
         packages = ['dcfe'],
         entry_points = {
             'console_scripts': [
