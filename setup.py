5533fc1f3f8ad974c0d1a18afcbc0fc491a06e01
lukas.a.pellegrini@gmail.com <lukas.a.pellegrini@gmail.com>
2021-02-27 22:34:56 +0100

    d

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
         name = 'dcfe',
-        version = '0.1.1',                
+        version = '0.1.2',                
         packages = ['dcfe'],
         entry_points = {
             'console_scripts': [
