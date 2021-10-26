8402a24b90d53663769177af938170c67cfd2df8
onshoremanover <lukas.a.pellegrini@gmail.com>
2021-10-26 15:00:11 +0200

    import versions

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-pip3 install plotext
-pip3 install uniplot
-pip3 install matplotlib
+pip3 install plotext==3.1.3
+pip3 install uniplot==0.4.4
+pip3 install matplotlib==3.4.3
+pip3 install numpy==1.20.2
 pip3 install -e .
 dcfe
